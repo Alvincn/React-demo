3cdfebb217a22afc2d47dd348c9d71e8eaec7045
Alvincn <2289530573@qq.com>
2023-05-05 21:38:17 +0800

    react demo

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -1,17 +1,29 @@
 import React, {Component} from 'react';
 import './index.css'
 class Item extends Component {
+    state={
+        mouse:false
+    }
+    handleMouse =(flag)=>{
+        return ()=>{
+            this.setState(
+                {mouse:flag}
+            )
+        }
+    }
+    handleCheck = ()=>{
 
+    }
     render() {
         const {todo} = this.props
         return (
             <div>
-                <li>
+                <li style={{backgroundColor:this.state.mouse?'#ddd':'white'}} onMouseMove={this.handleMouse(true)} onMouseLeave={this.handleMouse(false)}>
                     <label>
-                        <input type="checkbox" defaultChecked={todo.done}/>
+                        <input type="checkbox" defaultChecked={todo.done} onChange={this.handleCheck}/>
                         <span>{todo.name}</span>
                     </label>
-                    <button className="btn btn-danger" style={{display: 'none'}}>删除</button>
+                    <button  className="btn btn-danger" style={{display: this.state.mouse?'block':'none'}}>删除</button>
                 </li>
             </div>
         );
